b6d92abe37ba6a9ae5ec96288f154bb80176e60a
WillyGod <122815227+ElWillyGod@users.noreply.github.com>
2023-06-19 14:40:50 -0300

    Update aplicaciones.cpp

--- src/aplicaciones.cpp
+++ src/aplicaciones.cpp
@@ -1,5 +1,20 @@
 #include "../include/aplicaciones.h"
+#include "../include/colaDePrioridadPersona.h"
+#include "../include/tablaPersonas.h"
 
 void listarEnOrden(TTablaPersonas t, char** nombres, nat n){
 
+TColaDePrioridadPersona resultado = crearCP(MAX_ID);
+
+for(nat i = 0; i<n; i++){
+    if(perteneceATTablaPersonas(t,nombres[i])){
+        insertarEnCP(copiarTPersona(obtenerPersonaDeTTablaPersonas(t,nombres[i])), resultado);
+    }
+}
+while (!estaVaciaCP(resultado))
+{
+    imprimirTPersona(prioritaria(resultado));
+    eliminarPrioritaria(resultado);
+}
+liberarCP(resultado);
 }
